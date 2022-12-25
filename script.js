ba371104af03bb5844038d49c8446c51ae30d85b
Jyoti-Mish79 <jyotimishra2996@gmail.com>
2022-12-25 11:20:19 +0530

    sol

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let string = document.getElementById("string");
+let length = document.getElementById("length");
+string.addEventListener('keyup',()=>{
+    let str = string.value;
+    length.innerText = str.length;
+
+})
